bbc1f8cf454c8159c293e60f7e812df7b23e09cc
adhitia_dharma_putri <adhitiaputri16@gmail.com>
2021-05-04 09:33:30 +0700

    Memproses data internet sebagai JSON

--- app/src/main/java/org/d3if4037/galerihewan/model/Hewan.kt
+++ app/src/main/java/org/d3if4037/galerihewan/model/Hewan.kt
@@ -1,7 +1,9 @@
 package org.d3if4037.galerihewan.model
 
+import org.d3if4037.galerihewan.R
+
 data class Hewan(
         val nama: String,
         val namaLatin: String,
-        val imaageResId: Int
+        val imaageResId: Int = R.drawable.angsa
 )
